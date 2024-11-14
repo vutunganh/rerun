fe68155ba4f140193aa470c391392e6c8eec81df
Andreas Reich <r_andreas2@web.de>
2024-11-14 16:49:56 +0100

    remove todo on timeline component ui - better safe than sorry and keep it

--- crates/viewer/re_component_ui/src/timeline.rs
+++ crates/viewer/re_component_ui/src/timeline.rs
@@ -3,7 +3,6 @@ use re_types_core::ComponentBatch as _;
 use re_viewer_context::external::re_log_types::TimelineName;
 use re_viewer_context::{MaybeMutRef, ViewerContext};
 
-//TODO(#7498): might be unneeded after the dataframe view update
 pub(crate) fn edit_timeline_name(
     ctx: &ViewerContext<'_>,
     ui: &mut egui::Ui,
